8f300ee57eef501d6df53c07459babc293e65d6b
haimavatinandanpati <122ee0607@nitrkl.ac.in>
2023-05-14 23:38:47 +0530

    added opencv.py

--- opencv.py
+++ opencv.py
@@ -1,29 +1,31 @@
 import cv2
 
-# Load the image
+# Loading of the image
 image = cv2.imread(r"C:\Users\Haimavatinandan\Downloads\IP_BASICS.jpg")
 
-# Define the text to display
+# assigning the variables the data to be printed
 name = 'HAIMAVATINANDAN PATI'
 roll_number = '122EE0607'
 version = cv2.__version__
 text = f'{name} | {roll_number} | OpenCV {version}'
 
-# Set the font properties
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 0.8
-font_thickness = 2
+# Setting of the font and size of text
+font = cv2.FONT_HERSHEY_TRIPLEX
+font_scale = .6
+font_thickness = 1
 
 # Get the size of the text
 text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
 
-# Calculate the position of the text in the bottom right corner
-text_position = (image.shape[1] - text_size[0] - 10, image.shape[0] - 10)
+# Calculating the position of the text in the bottom right corner
+text_position = (730, 750)
 
-# Add the text to the image
-cv2.putText(image, text, text_position, font, font_scale, (0, 0, 255), font_thickness, cv2.LINE_AA)
+# Adding the text to the image
+cv2.putText(image, text, text_position, font, font_scale, (60, 0, 250), font_thickness, cv2.LINE_AA)
 
-# Display the image
+# Displaying the image
 cv2.imshow('Image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+print(image.shape)
+
